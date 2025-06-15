3bf4b447d50d895fa12f3bda0fbf67d0222c76e9
Alfr3si <luisalfredolopez510@gmail.com>
2025-06-15 01:56:59 -0600

    added documentation

---
+++ README.md
@@ -0,0 +1,42 @@
+# Description
+
+This repository was created and adapted for my workflow, but everyone interested is free to copy and modify it for their own use.
+
+# Images
+
+![image](Img/Neovim.png)
+![image2](Img/config.png)
+![image2](Img/code.png)
+![image2](Img/git.png)
+
+# plugins
+
+- [cmp.lua](https://github.com/hrsh7th/nvim-cmp)
+- [colorizer.lua](https://github.com/NvChad/nvim-colorizer.lua)
+- [colorscheme.lua](https://github.com/folke/tokyonight.nvim) _(Tokyonight)_
+- [comment.lua](https://github.com/numToStr/Comment.nvim)
+- [conform.lua](https://github.com/stevearc/conform.nvim)
+- [fugitive.lua](https://github.com/tpope/vim-fugitive)
+- [gitsigns.lua](https://github.com/lewis6991/gitsigns.nvim)
+- [lualine.lua](https://github.com/nvim-lualine/lualine.nvim)
+- [luasnip.lua](https://github.com/L3MON4D3/LuaSnip)
+- [markdown-preview.lua](https://github.com/iamcco/markdown-preview.nvim)
+- [mason.lua](https://github.com/williamboman/mason.nvim)
+- [render-markdown.lua](https://github.com/mehalter/markdown-render.nvim)
+- [snacks.lua](https://github.com/catgoose/snacks.nvim)
+- [todo_comments.lua](https://github.com/folke/todo-comments.nvim)
+- [treesiter.lua](https://github.com/nvim-treesitter/nvim-treesitter)
+
+# How to install
+
+## Requeriments
+
+- git
+- lazy.nvim
+- fzf or fd
+- ripgrep
+- nodejs
+
+## clone the repository
+
+    https://github.com/Alfr3si/nvim.git
--- lua/plugins/settings/cmp.lua
+++ lua/plugins/settings/cmp.lua
@@ -6,12 +6,12 @@ local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local npairs = require("nvim-autopairs")
 
 npairs.setup({
-  check_ts = true,
-  ts_config = {
-    lua = {'string', 'source'},
-    javascript = {'string', 'template_string'},
-  },
-  disable_filetype = { "TelescopePrompt", "spectre_panel" },
+	check_ts = true,
+	ts_config = {
+		lua = { "string", "source" },
+		javascript = { "string", "template_string" },
+	},
+	disable_filetype = { "TelescopePrompt", "spectre_panel" },
 })
 
 -- Vincula el evento "confirm_done" de cmp para que nvim-autopairs inserte el cierre adecuado
@@ -20,71 +20,79 @@ cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 local M = {}
 
 function M.setup()
-  cmp.setup({
-    snippet = {
-      expand = function(args)
-        luasnip.lsp_expand(args.body)
-      end,
-    },
-    window = {
-       completion = {
-        border = "rounded",
-       },
-       documentation = {
-         border = "rounded",
-       },
-    },
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				luasnip.lsp_expand(args.body)
+			end,
+		},
+		window = {
+			completion = {
+				border = "rounded",
+			},
+			documentation = {
+				border = "rounded",
+			},
+		},
 
-    mapping = {
-      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<C-j>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.choice_active() then
-          luasnip.change_choice(1)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<C-k>"] = cmp.mapping.select_prev_item(),
-      ["<CR>"] = cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      }),
-      ["<C-space>"] = cmp.mapping.complete(),
-    },
-    sources = {
-      { name = "nvim_lsp" },
-      { name = "path" },
-      { name = "luasnip" },
-      { name = "buffer" },
-    },
-  })
+		mapping = {
+			["<C-d>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-e>"] = cmp.mapping.abort(),
+			["<C-j>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif luasnip.choice_active() then
+					luasnip.change_choice(1)
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+			["<C-k>"] = cmp.mapping.select_prev_item(),
+			["<CR>"] = cmp.mapping.confirm({
+				behavior = cmp.ConfirmBehavior.Insert,
+				select = true,
+			}),
+			["<C-space>"] = cmp.mapping.complete(),
+		},
+		sources = {
+			{ name = "nvim_lsp" },
+			{ name = "path" },
+			{ name = "luasnip" },
+			{ name = "buffer" },
+		},
+	})
 
-  cmp.setup.filetype("gitcommit", {
-    sources = cmp.config.sources({
-      { name = "git" },
-      { name = "buffer" },
-    }),
-  })
+	cmp.setup.filetype("gitcommit", {
+		sources = cmp.config.sources({
+			{ name = "git" },
+			{ name = "buffer" },
+		}),
+	})
 
-  cmp.setup.cmdline({"/", "?"}, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = "buffer" },
-    },
-  })
+	cmp.setup.filetype("markdown", {
+		sources = cmp.config.sources({
+			{ name = "render-markdown" },
+		}, {
+			{ name = "buffer" },
+		}),
+	})
 
-  cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = "path" },
-    }, {
-      { name = "cmdline" },
-    }),
-  })
+	cmp.setup.cmdline({ "/", "?" }, {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = {
+			{ name = "buffer" },
+		},
+	})
+
+	cmp.setup.cmdline(":", {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = cmp.config.sources({
+			{ name = "path" },
+		}, {
+			{ name = "cmdline" },
+		}),
+	})
 end
 
 return M
