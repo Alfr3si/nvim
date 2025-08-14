13241e3c2b7f9c8b92854f19b3fa2e8e87b90bb7
Alfr3si <luisalfredolopez510@gmail.com>
2025-08-14 15:42:34 -0600

    archived: this configuration is set up with old lspconfig

---
+++ lua/core/diagnostic.lua
@@ -0,0 +1,17 @@
+vim.diagnostic.config({
+	virtual_lines = {
+		current_line = true,
+	},
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰚌 ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+			[vim.diagnostic.severity.HINT] = "󱧣 ",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
+		},
+	},
+})
